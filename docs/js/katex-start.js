802aab2736fef5ccc981af3c2a2b6dec3dd07ab9
Takahiro Ueda <tueda@nikhef.nl>
2017-12-09 10:15:35 +0100

    Change for pymdown-extensions 4.7.0

--- docs/js/katex-start.js
+++ docs/js/katex-start.js
@@ -1,5 +1,1 @@
 renderMathInElement(document.body);
-var items = document.getElementsByClassName("MathJax_Preview");
-for (var i = 0; i < items.length; i++) {
-  katex.render(items[i].innerText, items[i]);
-}
